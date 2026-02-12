1061b3ba08226c2efdf238ce73831deb4c956468
JordiCasas87 <Jordicasas87@gmail.com>
2026-02-12 20:24:43 +0100

    feat: añadido un import en vite config , para que funcioe render

--- wolves-frontend/wolves-frontend/vite.config.js
+++ wolves-frontend/wolves-frontend/vite.config.js
@@ -1,19 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: [
+      'wolves-game-front.onrender.com'
+    ]
+  }
+})
+//hola
+  /*
+   ─────────────────────────────────────────────
+   🔴 CONFIGURACIÓN LOCAL (BACKEND EN LOCAL)
+   👉 Úsala SOLO si arrancas IntelliJ en :8080
+   👉 Para activrrla: descomenta este bloque
+   ─────────────────────────────────────────────
+
   server: {
     proxy: {
-      // Auth endpoints live under /api
       '/api': 'http://localhost:8080',
-
-      // These are protected by JWT; proxy here avoids CORS in dev.
       '/game': 'http://localhost:8080',
       '/players': 'http://localhost:8080',
       '/questions': 'http://localhost:8080',
       '/me': 'http://localhost:8080',
     },
   },
-})
+
+  */
+
+  /*
+   ─────────────────────────────────────────────
+   🟢 CONFIGURACIÓN ACTIVA (RENDER)
+   👉 No hace falta server ni proxy
+   👉 Las llamadas van a VITE_API_URL
+   👉 Definido en .env
+   ─────────────────────────────────────────────
+  */
